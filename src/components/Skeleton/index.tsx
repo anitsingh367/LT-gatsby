afb8ec550a4ea93bfd6a3f7bee5c6355566e4792
Anit Singh <anitsingh367@gmail.com>
2024-09-04 12:20:28 +0530

    folder name casing

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,19 @@
+//import npm packges
+import React from 'react'
+import Skeleton from "@mui/material/Skeleton";
+
+//import styles
+import { Container } from "@mui/material";
+
+const SkeletonCard = (props) => {
+  return (
+    <Container sx={{ width: "350px" }}>
+      <Skeleton width="100%" height="300px" />
+      <Skeleton width="100%" height="20px" />
+      <Skeleton width="100%" height="40px" />
+      <Skeleton width="100%" height="20px" />
+    </Container>
+  );
+};
+
+export default SkeletonCard;
